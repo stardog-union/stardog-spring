ba7ef27c52ad4638af3a5fbc06ad9de7a0cd1e7e
AlBaker <albaker@clarkparsia.com>
2014-04-21 22:42:56 -0400

    Separate out spring batch support, and move to separate project directories

---
+++ stardog-spring/src/test/java/com/complexible/stardog/ext/spring/EmbeddedProvider.java
@@ -0,0 +1,40 @@
+package com.complexible.stardog.ext.spring;
+
+import com.complexible.common.protocols.server.ServerException;
+import com.complexible.stardog.Stardog;
+import com.complexible.stardog.StardogException;
+import com.complexible.stardog.api.admin.AdminConnection;
+import com.complexible.stardog.api.admin.AdminConnectionConfiguration;
+import com.complexible.stardog.protocols.snarl.SNARLProtocolConstants;
+
+/**
+ * Created by albaker on 3/4/14.
+ */
+public class EmbeddedProvider implements Provider {
+
+    @Override
+    public void execute(String to, String url, String user, String pass) {
+
+        try {
+            Stardog.buildServer()
+                    .bind(SNARLProtocolConstants.EMBEDDED_ADDRESS)
+                    .start();
+            AdminConnection dbms = AdminConnectionConfiguration.toEmbeddedServer().credentials(user, pass).connect();
+            if (dbms.list().contains(to)) {
+                dbms.drop(to);
+                dbms.createMemory(to);
+            } else {
+                dbms.createMemory(to);
+            }
+            dbms.close();
+        } catch (StardogException e) {
+
+
+        } catch (ServerException e) {
+
+        } finally {
+
+        }
+
+    }
+}
